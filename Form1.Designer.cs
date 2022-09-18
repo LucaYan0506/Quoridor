3b8e65182440bcc8a239a4d41da345c26086d0f6
Luca Yan <lucayan0506>
2022-09-18 21:35:13 +0100

    walls feature done

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,11 +33,15 @@ namespace Quoridor
             this.label1 = new System.Windows.Forms.Label();
             this.HorizontalLine = new System.Windows.Forms.PictureBox();
             this.VerticalLine = new System.Windows.Forms.PictureBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.playerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.playerToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.wallsLeftLbl = new System.Windows.Forms.Label();
             this.pawnTurn = new Quoridor.PawnPictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.HorizontalLine)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.VerticalLine)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pawnTurn)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,7 +49,7 @@ namespace Quoridor
             // 
             this.gridPanel.BackgroundImage = global::Quoridor.Properties.Resources.grid;
             this.gridPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.gridPanel.Location = new System.Drawing.Point(0, 0);
+            this.gridPanel.Location = new System.Drawing.Point(0, 29);
             this.gridPanel.Name = "gridPanel";
             this.gridPanel.Size = new System.Drawing.Size(600, 603);
             this.gridPanel.TabIndex = 1;
@@ -55,7 +59,7 @@ namespace Quoridor
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(606, 9);
+            this.label1.Location = new System.Drawing.Point(606, 38);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(110, 25);
             this.label1.TabIndex = 2;
@@ -64,7 +68,7 @@ namespace Quoridor
             // HorizontalLine
             // 
             this.HorizontalLine.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(67)))), ((int)(((byte)(33)))));
-            this.HorizontalLine.Location = new System.Drawing.Point(611, 109);
+            this.HorizontalLine.Location = new System.Drawing.Point(611, 158);
             this.HorizontalLine.Name = "HorizontalLine";
             this.HorizontalLine.Size = new System.Drawing.Size(134, 14);
             this.HorizontalLine.TabIndex = 0;
@@ -76,7 +80,7 @@ namespace Quoridor
             // VerticalLine
             // 
             this.VerticalLine.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(67)))), ((int)(((byte)(33)))));
-            this.VerticalLine.Location = new System.Drawing.Point(611, 140);
+            this.VerticalLine.Location = new System.Drawing.Point(611, 189);
             this.VerticalLine.Name = "VerticalLine";
             this.VerticalLine.Size = new System.Drawing.Size(13, 134);
             this.VerticalLine.TabIndex = 4;
@@ -85,28 +89,53 @@ namespace Quoridor
             this.VerticalLine.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Line_MouseMove);
             this.VerticalLine.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Line_MouseUp);
             // 
-            // label2
+            // menuStrip1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(630, 308);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "label2";
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.playerToolStripMenuItem,
+            this.playerToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(785, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
             // 
-            // button1
+            // newGameToolStripMenuItem
             // 
-            this.button1.Location = new System.Drawing.Point(629, 372);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // playerToolStripMenuItem
+            // 
+            this.playerToolStripMenuItem.Name = "playerToolStripMenuItem";
+            this.playerToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.playerToolStripMenuItem.Text = "2 Player";
+            this.playerToolStripMenuItem.Click += new System.EventHandler(this.playerToolStripMenuItem_Click_1);
+            // 
+            // playerToolStripMenuItem1
+            // 
+            this.playerToolStripMenuItem1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.playerToolStripMenuItem1.Name = "playerToolStripMenuItem1";
+            this.playerToolStripMenuItem1.Size = new System.Drawing.Size(60, 20);
+            this.playerToolStripMenuItem1.Text = "4 Player";
+            this.playerToolStripMenuItem1.Click += new System.EventHandler(this.playerToolStripMenuItem1_Click);
+            // 
+            // wallsLeftLbl
+            // 
+            this.wallsLeftLbl.AutoSize = true;
+            this.wallsLeftLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wallsLeftLbl.Location = new System.Drawing.Point(609, 63);
+            this.wallsLeftLbl.Name = "wallsLeftLbl";
+            this.wallsLeftLbl.Size = new System.Drawing.Size(93, 20);
+            this.wallsLeftLbl.TabIndex = 8;
+            this.wallsLeftLbl.Text = "(10 wall left)";
             // 
             // pawnTurn
             // 
-            this.pawnTurn.Location = new System.Drawing.Point(611, 37);
+            this.pawnTurn.Location = new System.Drawing.Point(611, 86);
             this.pawnTurn.Name = "pawnTurn";
             this.pawnTurn.Size = new System.Drawing.Size(54, 54);
             this.pawnTurn.TabIndex = 3;
@@ -116,18 +145,21 @@ namespace Quoridor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(785, 600);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(785, 635);
+            this.Controls.Add(this.wallsLeftLbl);
             this.Controls.Add(this.VerticalLine);
             this.Controls.Add(this.HorizontalLine);
             this.Controls.Add(this.pawnTurn);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.gridPanel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.HorizontalLine)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.VerticalLine)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pawnTurn)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -141,7 +173,10 @@ namespace Quoridor
         private PawnPictureBox pawnTurn;
         private System.Windows.Forms.PictureBox HorizontalLine;
         private System.Windows.Forms.PictureBox VerticalLine;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem playerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem playerToolStripMenuItem1;
+        private System.Windows.Forms.Label wallsLeftLbl;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,8 @@ namespace Quoridor
     public partial class Form1 : Form
     {
         //component to init the game
+        bool four_people = false;
+        
         Tuple<int, int>[] dirs = new Tuple<int,int>[4];
         PawnPictureBox[] directions = new PawnPictureBox[4];
 
@@ -23,16 +25,15 @@ namespace Quoridor
         TriangularPictureBox[] endArea = new TriangularPictureBox[4];
 
         //grid
-        Grid grid = new Grid();
+        Grid grid;
         Point[,] barrier = new Point[8, 8];
         Point[,] barrier1 = new Point[8, 8];
         Point[,] barrier2 = new Point[8, 8];
 
-
         //component to make the game turn-based
-        int turn = 0;
+        int turn;
         Point orginalLocation;
-
+        int[] wallsLeft;
 
         //components for verticalLine and horizontalLine
         PictureBox curr;
@@ -46,6 +47,16 @@ namespace Quoridor
 
         private void InitGame()
         {
+            //clear everything in gridPanel
+            gridPanel.Controls.Clear();
+            
+            //reset turns and walls left
+            turn = 0;
+            wallsLeft = new int[]{10, 10, 10, 10};
+
+            //new grid
+            grid = new Grid();
+
             //initialize endArea
             Image[] backgroundImages = { Properties.Resources.greenBackground, Properties.Resources.redBackground, Properties.Resources.blueBackground, Properties.Resources.yellowbackground };
             Point[] locations = { new Point(2, 0), new Point(534, 0), new Point(2, 536), new Point(0,0) };
@@ -121,10 +132,10 @@ namespace Quoridor
 
             //init dirs
             int distance = 67;
-            dirs[0] = new Tuple<int, int>(distance, 0);
-            dirs[1] = new Tuple<int, int>(-distance, 0);
-            dirs[2] = new Tuple<int, int>(0, distance);
-            dirs[3] = new Tuple<int, int>(0, -distance);
+            dirs[0] = new Tuple<int, int>(distance, 0);  //right
+            dirs[1] = new Tuple<int, int>(-distance, 0); //left
+            dirs[2] = new Tuple<int, int>(0, distance);  //bot
+            dirs[3] = new Tuple<int, int>(0, -distance); //top
 
             //initialize pawns
             Image[] PawnImages = { Image.FromFile("..\\..\\Resources\\greenPawn.png"), Image.FromFile("..\\..\\Resources\\redPawn.png"), Image.FromFile("..\\..\\Resources\\bluePawn.png"), Image.FromFile("..\\..\\Resources\\yellowPawn.png") };
@@ -159,11 +170,11 @@ namespace Quoridor
             //init grid barrier
             for (int i = 0; i < barrier1.GetLength(0); i++)
                 for (int j = 0; j < barrier1.GetLength(1); j++)
-                    barrier1[i, j] = new Point(61 + (j * 66), 0 + (i * 67));
+                    barrier1[i, j] = new Point(61 + (j * 66), 29 + (i * 67));
 
             for (int i = 0; i < barrier2.GetLength(0); i++)
                 for (int j = 0; j < barrier2.GetLength(1); j++)
-                    barrier2[i, j] = new Point(0 + (j * 66), 61 + (i * 67));
+                    barrier2[i, j] = new Point(0 + (j * 66), 90 + (i * 67));
         }
 
         private int[] location_to_indexForPawns(Point location)
@@ -183,11 +194,27 @@ namespace Quoridor
 
         private void PawnImg_Click(object sender, EventArgs e)
         {
+            //disable the feature to choose the mode (2 players or 4 players)
+            playerToolStripMenuItem1.Enabled = false;
+            playerToolStripMenuItem.Enabled = false;
+
             //if it isn't his turn, then don't allow him to click pawn.
-            if ( int.Parse(((PawnPictureBox)sender).Name.Substring(5)) != turn)
+            if (int.Parse(((PawnPictureBox)sender).Name.Substring(5)) != turn)
                 return;
 
             currPawn = (PawnPictureBox)sender;
+
+            int[] blockIndex = location_to_indexForPawns(new Point(currPawn.Location.X, currPawn.Location.Y));
+
+            //borders
+            Border[] borders =
+            {
+                grid.Blocks[blockIndex[0],blockIndex[1]].BorderRight,
+                grid.Blocks[blockIndex[0],blockIndex[1]].BorderLeft,
+                grid.Blocks[blockIndex[0],blockIndex[1]].BorderBot,
+                grid.Blocks[blockIndex[0],blockIndex[1]].BorderTop,
+            };
+
 
             //generate "grey pawn" (dirs) 
             for (int i = 0; i < directions.Length; i++)
@@ -196,7 +223,7 @@ namespace Quoridor
 
                 Point loc = new Point(currPawn.Location.X + dirs[i].Item1, currPawn.Location.Y + dirs[i].Item2);
                 int[] indexes = location_to_indexForPawns(loc);
-                if (!outside_bound(indexes[0],grid.Blocks.GetLength(0)) && !outside_bound(indexes[1], grid.Blocks.GetLength(1)))
+                if (!outside_bound(indexes[0],grid.Blocks.GetLength(0)) && !outside_bound(indexes[1], grid.Blocks.GetLength(1)) && borders[i].isEmpty)
                 {
                     //if there isn't any pawn in the cell
                     if (grid.Blocks[indexes[0], indexes[1]].isEmpty)
@@ -212,6 +239,8 @@ namespace Quoridor
                         {
                             loc = new Point(currPawn.Location.X + dirs[i].Item1 * index, currPawn.Location.Y + dirs[i].Item2 * index);
                             indexes = location_to_indexForPawns(loc);
+
+                            index++;
 
                             if (outside_bound(indexes[0], grid.Blocks.GetLength(0)) || outside_bound(indexes[1], grid.Blocks.GetLength(1)))
                             {
@@ -247,9 +276,15 @@ namespace Quoridor
             grid.Blocks[indexes[0], indexes[1]].isEmpty = false;
 
             hide_directions();
-            turn = (turn + 1) % Pawns.Length;
+            if (four_people)
+                turn = (turn + 1) % Pawns.Length;
+            else
+                turn = (turn + 2) % Pawns.Length;
 
+            //update image
             pawnTurn.Image = Pawns[turn].Image;
+            //update walls left
+            wallsLeftLbl.Text = $"({wallsLeft[turn]} wall left)";
         }
 
         private void hide_directions()
@@ -263,20 +298,34 @@ namespace Quoridor
             hide_directions();
         }
 
-        private int[] location_to_indexForBarrier(Point location)
+        private int[] location_to_indexForBarrierMouse(Point location)
         {
             int[] res = new int[2];
 
             res[0] = (int)Math.Round((location.Y - 32) / 66.0) - 1;
             res[1] = (int)Math.Round((location.X - 75) / 68.0);
 
-            label2.Text = location.ToString() + "\n" + " " + res[0] + " " + res[1];
+            return res;
+        }
+        private int[] location_to_indexForBarrierLine(Point location)
+        {
+            int[] res = new int[2];
+
+            res[0] = (location.Y / 67);
+            res[1] = (location.X / 66);
 
             return res;
         }
 
         private void Line_MouseDown(object sender, MouseEventArgs e)
         {
+            //disable the feature to choose the mode (2 players or 4 players)
+            playerToolStripMenuItem1.Enabled = false;
+            playerToolStripMenuItem.Enabled = false;
+
+            //hide all grey pans (if exists)
+            hide_directions();
+
             if (e.Button == MouseButtons.Right)
                 return;
 
@@ -295,36 +344,122 @@ namespace Quoridor
 
         private void Line_MouseUp(object sender, MouseEventArgs e)
         {
-            PictureBox newLine = new PictureBox();
-            newLine.BackColor = ((PictureBox)sender).BackColor;
-            newLine.Location = ((PictureBox)sender).Location;
-            newLine.Size = ((PictureBox)sender).Size;
-
-            gridPanel.Controls.Add(newLine);
-            newLine.BringToFront();
+            //location that the new line will be (in the form & in the grid/backend)
+            Point lineLocation = new Point(((PictureBox)sender).Location.X, ((PictureBox)sender).Location.Y - 29);
 
             ((PictureBox)sender).Location = orginalLocation;
             move = false;
+
+            Point MouseLocation = new Point(MousePosition.X - this.Location.X, MousePosition.Y - this.Location.Y - 29);
+            int[] indexes = location_to_indexForBarrierMouse(MouseLocation);
+
+            //if he stil has some walls left
+            if (wallsLeft[turn] < 1)
+            {
+                MessageBox.Show("No walls left","Quoridor",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                return;
+            }
+
+            //if it's not outside the bound and he has some walls left
+            if (!outside_bound(indexes[0], barrier.GetLength(0)) && !outside_bound(indexes[1], barrier.GetLength(1)))
+            {
+                //generate the line in the grid (in the backend) if the line is valid
+                int[] blockIndex = location_to_indexForBarrierLine(lineLocation);
+              
+                if (((PictureBox)sender).Name == "HorizontalLine")
+                {
+                    //check that there isn't another line in that block
+                    if (grid.Blocks[blockIndex[0], blockIndex[1]].BorderBot.isEmpty == false ||
+                        grid.Blocks[blockIndex[0] + 1, blockIndex[1]].BorderTop.isEmpty == false ||
+                        grid.Blocks[blockIndex[0], blockIndex[1] + 1].BorderBot.isEmpty == false ||
+                        grid.Blocks[blockIndex[0] + 1, blockIndex[1] + 1].BorderTop.isEmpty == false ||
+                        (grid.Blocks[blockIndex[0], blockIndex[1]].BorderRight.isEmpty == false && grid.Blocks[blockIndex[0] + 1, blockIndex[1]].BorderRight.isEmpty == false))
+                        return;
+
+                    grid.Blocks[blockIndex[0], blockIndex[1]].BorderBot.isEmpty = false;
+                    grid.Blocks[blockIndex[0] + 1, blockIndex[1]].BorderTop.isEmpty = false;
+
+                    grid.Blocks[blockIndex[0], blockIndex[1] + 1].BorderBot.isEmpty = false;
+                    grid.Blocks[blockIndex[0] + 1, blockIndex[1] + 1].BorderTop.isEmpty = false;
+                }
+                else
+                {
+                    //check that there isn't another line in that block
+                    if (grid.Blocks[blockIndex[0], blockIndex[1]].BorderRight.isEmpty == false ||
+                        grid.Blocks[blockIndex[0], blockIndex[1] + 1].BorderLeft.isEmpty == false ||
+                        grid.Blocks[blockIndex[0] + 1, blockIndex[1]].BorderRight.isEmpty == false ||
+                        grid.Blocks[blockIndex[0] + 1, blockIndex[1] + 1].BorderLeft.isEmpty == false ||
+                        (grid.Blocks[blockIndex[0], blockIndex[1]].BorderBot.isEmpty == false && grid.Blocks[blockIndex[0], blockIndex[1] + 1].BorderBot.isEmpty == false))
+                        return;
+                    
+                    grid.Blocks[blockIndex[0], blockIndex[1]].BorderRight.isEmpty = false;
+                    grid.Blocks[blockIndex[0], blockIndex[1] + 1].BorderLeft.isEmpty = false;
+
+                    grid.Blocks[blockIndex[0] + 1, blockIndex[1]].BorderRight.isEmpty = false;
+                    grid .Blocks[blockIndex[0] + 1, blockIndex[1] + 1].BorderLeft.isEmpty = false;
+                }
+
+                //generate the line (in the form)
+                PictureBox newLine = new PictureBox();
+                newLine.BackColor = ((PictureBox)sender).BackColor;
+                newLine.Location = lineLocation;
+                newLine.Size = ((PictureBox)sender).Size;
+
+                gridPanel.Controls.Add(newLine);
+                newLine.BringToFront();
+
+                //decrese walls left
+                wallsLeft[turn]--;
+
+                //next turn
+                if (four_people)
+                    turn = (turn + 1) % Pawns.Length;
+                else
+                    turn = (turn + 2) % Pawns.Length;
+                //update image
+                pawnTurn.Image = Pawns[turn].Image;
+                //update walls left
+                wallsLeftLbl.Text = $"({wallsLeft[turn]} wall left)";
+            }
         }
 
         private void Line_MouseMove(object sender, MouseEventArgs e)
         {
             if (move)
             {
-                Point MouseLocation = new Point(MousePosition.X - this.Location.X, MousePosition.Y - this.Location.Y);
-                int[] indexes = location_to_indexForBarrier(MouseLocation);
+                Point MouseLocation = new Point(MousePosition.X - this.Location.X, MousePosition.Y - this.Location.Y - 29);
+                int[] indexes = location_to_indexForBarrierMouse(MouseLocation);
 
-                if (!outside_bound(indexes[0], barrier.GetLength(0)))
-                    curr.Location = new Point (curr.Location.X, barrier[indexes[0], 0].Y);
-
-                if (!outside_bound(indexes[1], barrier.GetLength(1)))
-                      curr.Location = new Point(barrier[0, indexes[1]].X, curr.Location.Y);
+                if (outside_bound(indexes[0], barrier.GetLength(0)) || outside_bound(indexes[1], barrier.GetLength(1)))
+                    curr.Location = orginalLocation;
+                else
+                    curr.Location = new Point(barrier[indexes[0], indexes[1]].X, barrier[indexes[0], indexes[1]].Y);
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void playerToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(grid.Blocks[0, 0].BorderBot.isEmpty.ToString());
+            playerToolStripMenuItem1.BackColor = SystemColors.ActiveCaption;
+            playerToolStripMenuItem.BackColor = SystemColors.Control;
+
+            four_people = true;
+        }
+
+        private void playerToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            playerToolStripMenuItem.BackColor = SystemColors.ActiveCaption;
+            playerToolStripMenuItem1.BackColor = SystemColors.Control;
+
+            four_people = false;
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //enable the feature to choose the mode (2 players or 4 players)
+            playerToolStripMenuItem1.Enabled = true;
+            playerToolStripMenuItem.Enabled = true;
+
+            InitGame();
         }
     }
 }
